2b39863bb04813e1f7951ee0093cf05fbf2650af
Pedro Duarte <contact@peduarte.com>
2020-07-31 15:55:55 +0200

    Content

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,10 @@
 import Head from 'next/head';
-import { Box, Heading, Flex, Container, Button, Divider, Subheading } from '@modulz/radix';
+import { Box, Text, Heading, Flex, Container, Button, Divider, Subheading, Grid } from '@modulz/radix';
 import { styled } from '../css';
 
 const buttonStyles = {
   appearance: 'none',
-  borderWidth: '1px',
-  borderStyle: 'solid',
-  borderColor: 'black',
+  border: '1px solid black',
   backgroundColor: 'black',
   color: 'white',
   padding: '12px',
@@ -82,51 +80,34 @@ export default function Home() {
 
       <Box sx={{ bg: 'gray900', py: 6, my: 9 }}>
         <Container size={3}>
-          <Flex sx={{ mb: 4 }}>
-            <Box sx={{ flex: '1 0 50%', mr: 3 }}>
+          <Grid sx={{ gap: 4, gridTemplateColumns: '1fr 1fr' }}>
+            <Box sx={{ flex: '1 0 50%' }}>
               <pre className="dark language-jsx">
-                {`const Button = styled('button', ${JSON.stringify(buttonStyles, null, 2)})`}
+                {`const Button = styled('button', ${JSON.stringify(buttonStyles, null, 2)}, ${JSON.stringify(
+                  buttonVariants,
+                  null,
+                  2
+                )}))`}
               </pre>
             </Box>
             <Flex
               sx={{
                 flex: '1 0 50%',
-                alignItems: 'center',
-                justifyContent: 'center',
+
                 bg: 'white',
                 p: 6,
-                ml: 3,
                 borderRadius: 2,
-              }}
-            >
-              <InteropButton>Button component</InteropButton>
-            </Flex>
-          </Flex>
-          <Flex>
-            <Box sx={{ flex: '1 0 50%', mr: 3 }}>
-              <pre className="dark language-jsx">
-                {`const Button = styled('button', …, ${JSON.stringify(buttonVariants, null, 2)})`}
-              </pre>
-            </Box>
-            <Flex
-              sx={{
-                flex: '1 0 50%',
-                alignItems: 'center',
                 justifyContent: 'center',
-                bg: 'white',
-                p: 6,
-                ml: 3,
-                borderRadius: 2,
+                alignItems: 'center',
               }}
             >
-              <InteropButton css={{ marginRight: '5px' }} variant="blue">
-                Button component
-              </InteropButton>
-              <InteropButton css={{ marginLeft: '5px' }} variant="red">
-                Button component
-              </InteropButton>
+              <Grid sx={{ gap: 2 }}>
+                <InteropButton>Default button</InteropButton>
+                <InteropButton variant="blue">Blue component</InteropButton>
+                <InteropButton variant="red">Red component</InteropButton>
+              </Grid>
             </Flex>
-          </Flex>
+          </Grid>
         </Container>
       </Box>
 
@@ -137,6 +118,196 @@ export default function Home() {
         <Heading as="h3" size={4} mb={4}>
           A fully-featured styling library.
         </Heading>
+      </Container>
+
+      <Container size={3} px={[null, 5, 9]}>
+        <Grid sx={{ gap: [5, 8], gridTemplateColumns: ['1fr ', '1fr 1fr'] }}>
+          <Box px={[8, 6, 8]} pt={6}>
+            <Heading size={2} mb={4}>
+              Performant
+            </Heading>
+            <Text as="p" size={4} sx={{ lineHeight: 3 }}>
+              The main motivation and criteria is to make a performant library
+            </Text>
+          </Box>
+
+          <Box px={[8, 6, 8]} pt={6}>
+            <Heading size={2} mb={4}>
+              Atomic
+            </Heading>
+            <Text as="p" size={4} sx={{ lineHeight: 3 }}>
+              Each CSS property is a an atomic part of your complete CSS
+            </Text>
+          </Box>
+
+          <Box px={[8, 6, 8]} pt={6}>
+            <Heading size={2} mb={4}>
+              Reusability
+            </Heading>
+            <Text as="p" size={4} sx={{ lineHeight: 3 }}>
+              Each CSS property, given the same screen, pseudo and value is considered the same, giving high degree of
+              reusability
+            </Text>
+          </Box>
+
+          <Box px={[8, 6, 8]} pt={6}>
+            <Heading size={2} mb={4}>
+              Optimal injection
+            </Heading>
+            <Text as="p" size={4} sx={{ lineHeight: 3 }}>
+              AKA Critical Path CSS. You can compose your styles outside of your UI, but no injection happens until it
+              is actually used. Only used variants are injected
+            </Text>
+          </Box>
+
+          <Box px={[8, 6, 8]} pt={6}>
+            <Heading size={2} mb={4}>
+              Screens
+            </Heading>
+            <Text as="p" size={4} sx={{ lineHeight: 3 }}>
+              Define a set of media queries as screens to easily express CSS active within a screen. Any type of @ rule
+              is supported, for example, touch screens or portrait orientation
+            </Text>
+          </Box>
+
+          <Box px={[8, 6, 8]} pt={6}>
+            <Heading size={2} mb={4}>
+              Utils
+            </Heading>
+            <Text as="p" size={4} sx={{ lineHeight: 3 }}>
+              Create your own CSS properties
+            </Text>
+          </Box>
+
+          <Box px={[8, 6, 8]} pt={6}>
+            <Heading size={2} mb={4}>
+              Typed
+            </Heading>
+            <Text as="p" size={4} sx={{ lineHeight: 3 }}>
+              Fully typed API, including variants
+            </Text>
+          </Box>
+
+          <Box px={[8, 6, 8]} pt={6}>
+            <Heading size={2} mb={4}>
+              No Specificity
+            </Heading>
+            <Text as="p" size={4} sx={{ lineHeight: 3 }}>
+              No more specificity issues as an atomic mindset opens up a more efficient and straight forward way to
+              solve it. Even extended components (via the styled or as prop) won't contain multiple CSS properties.
+            </Text>
+          </Box>
+
+          <Box px={[8, 6, 8]} pt={6}>
+            <Heading size={2} mb={4}>
+              Token-aware values
+            </Heading>
+            <Text as="p" size={4} sx={{ lineHeight: 3 }}>
+              Apply tokens as CSS values without any extra work
+            </Text>
+          </Box>
+
+          <Box px={[8, 6, 8]} pt={6}>
+            <Heading size={2} mb={4}>
+              Token-based theming
+            </Heading>
+            <Text as="p" size={4} sx={{ lineHeight: 3 }}>
+              Create themes overriding the tokens and expose themes to any parts of your app
+            </Text>
+          </Box>
+
+          <Box px={[8, 6, 8]} pt={6}>
+            <Heading size={2} mb={4}>
+              Tokens as CSS Variables
+            </Heading>
+            <Text as="p" size={4} sx={{ lineHeight: 3 }}>
+              Tokens are converted to CSS Variables, making dynamic themes performant to apply
+            </Text>
+          </Box>
+
+          <Box px={[8, 6, 8]} pt={6}>
+            <Heading size={2} mb={4}>
+              Variant support
+            </Heading>
+            <Text as="p" size={4} sx={{ lineHeight: 3 }}>
+              First class variant support, which get typed automatically
+            </Text>
+          </Box>
+
+          <Box px={[8, 6, 8]} pt={6}>
+            <Heading size={2} mb={4}>
+              Polymorphic As prop
+            </Heading>
+            <Text as="p" size={4} sx={{ lineHeight: 3 }}>
+              Included in Components returned from the styled function
+            </Text>
+          </Box>
+
+          <Box px={[8, 6, 8]} pt={6}>
+            <Heading size={2} mb={4}>
+              Responsive values
+            </Heading>
+            <Text as="p" size={4} sx={{ lineHeight: 3 }}>
+              CSS values can be defined per screen
+            </Text>
+          </Box>
+
+          <Box px={[8, 6, 8]} pt={6}>
+            <Heading size={2} mb={4}>
+              Responsive variants
+            </Heading>
+            <Text as="p" size={4} sx={{ lineHeight: 3 }}>
+              You can apply different variants per screens
+            </Text>
+          </Box>
+
+          <Box px={[8, 6, 8]} pt={6}>
+            <Heading size={2} mb={4}>
+              Override usage
+            </Heading>
+            <Text as="p" size={4} sx={{ lineHeight: 3 }}>
+              Similar to sx prop, allows overrides to be defined at the usage level
+            </Text>
+          </Box>
+
+          <Box px={[8, 6, 8]} pt={6}>
+            <Heading size={2} mb={4}>
+              Tiny
+            </Heading>
+            <Text as="p" size={4} sx={{ lineHeight: 3 }}>
+              Both the css and the styled libraries together weight less than 3kb gzipped
+            </Text>
+          </Box>
+
+          <Box px={[8, 6, 8]} pt={6}>
+            <Heading size={2} mb={4}>
+              Promotes consistent UI
+            </Heading>
+            <Text as="p" size={4} sx={{ lineHeight: 3 }}>
+              A especially good feature when building design systems
+            </Text>
+          </Box>
+
+          <Box px={[8, 6, 8]} pt={6}>
+            <Heading size={2} mb={4}>
+              Less generated CSS
+            </Heading>
+            <Text as="p" size={4} sx={{ lineHeight: 3 }}>
+              The total output of the generated CSS gets smaller as your app gets bigger
+            </Text>
+          </Box>
+
+          <Box px={[8, 6, 8]} pt={6}>
+            <Heading size={2} mb={4}>
+              Create Higher level APIs
+            </Heading>
+            <Text as="p" size={4} sx={{ lineHeight: 3 }}>
+              Can be created to fulfil different needs, for different frameworks. We can decide to also open source the
+              low level library and allow the community to also create different types of API's as a potential marketing
+              strategy. Same as React and React DOM.
+            </Text>
+          </Box>
+        </Grid>
       </Container>
     </div>
   );
