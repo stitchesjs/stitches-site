ef7de86421b88bc116add199a174ed2183a34ef0
Colm Tuite <colmtuite@gmail.com>
2020-08-27 01:28:53 +0100

    Add community links to nav.

--- layouts/index.tsx
+++ layouts/index.tsx
@@ -108,6 +108,71 @@ export default (frontMatter: FrontMatter) => {
                     ))}
                   </Box>
                 ))}
+                <Text
+                  as="h4"
+                  size="3"
+                  css={{
+                    fontWeight: 500,
+                    px: '5',
+                    py: '2',
+                  }}
+                >
+                  Community
+                </Text>
+                <Box
+                  as="a"
+                  href="https://github.com/modulz/stitches"
+                  target="_blank"
+                  css={{
+                    display: 'flex',
+                    alignItems: 'center',
+                    textDecoration: 'none',
+                    color: 'hiContrast',
+                    py: '2',
+                    px: '5',
+                    userSelect: 'none',
+                    minHeight: '6',
+                    transition: 'background-color 50ms linear',
+                    ':hover': {
+                      backgroundColor:'blue200',
+                    }
+                  }}>
+                  <Text size="2" css={{ color: 'inherit', lineHeight: '1' }}>
+                    Github
+                  </Text>
+                  <Flex css={{ ml: 1, color: 'gray500' }}>
+                    <svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+                      <path fill-rule="evenodd" clip-rule="evenodd" d="M3.64645 11.3536C3.45118 11.1583 3.45118 10.8417 3.64645 10.6465L10.2929 4L6 4C5.72386 4 5.5 3.77614 5.5 3.5C5.5 3.22386 5.72386 3 6 3L11.5 3C11.6326 3 11.7598 3.05268 11.8536 3.14645C11.9473 3.24022 12 3.36739 12 3.5L12 9.00001C12 9.27615 11.7761 9.50001 11.5 9.50001C11.2239 9.50001 11 9.27615 11 9.00001V4.70711L4.35355 11.3536C4.15829 11.5488 3.84171 11.5488 3.64645 11.3536Z" fill="currentColor"/>
+                    </svg>
+                  </Flex>
+                </Box>
+                <Box
+                  as="a"
+                  href="https://twitter.com/stitchesjs"
+                  target="_blank"
+                  css={{
+                    display: 'flex',
+                    alignItems: 'center',
+                    textDecoration: 'none',
+                    color: 'hiContrast',
+                    py: '2',
+                    px: '5',
+                    userSelect: 'none',
+                    minHeight: '6',
+                    transition: 'background-color 50ms linear',
+                    ':hover': {
+                      backgroundColor:'blue200',
+                    }
+                  }}>
+                  <Text size="2" css={{ color: 'inherit', lineHeight: '1' }}>
+                    Twitter
+                  </Text>
+                  <Flex css={{ ml: 1, color: 'gray500' }}>
+                    <svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+                      <path fill-rule="evenodd" clip-rule="evenodd" d="M3.64645 11.3536C3.45118 11.1583 3.45118 10.8417 3.64645 10.6465L10.2929 4L6 4C5.72386 4 5.5 3.77614 5.5 3.5C5.5 3.22386 5.72386 3 6 3L11.5 3C11.6326 3 11.7598 3.05268 11.8536 3.14645C11.9473 3.24022 12 3.36739 12 3.5L12 9.00001C12 9.27615 11.7761 9.50001 11.5 9.50001C11.2239 9.50001 11 9.27615 11 9.00001V4.70711L4.35355 11.3536C4.15829 11.5488 3.84171 11.5488 3.64645 11.3536Z" fill="currentColor"/>
+                    </svg>
+                  </Flex>
+                </Box>
               </Box>
             </ScrollArea>
           </Box>
