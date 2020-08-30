fad7bedf2c201f381db4f0595518cc1a09ab4d17
Colm Tuite <colmtuite@gmail.com>
2020-08-31 00:22:25 +0100

    Complete footer.

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,17 +1,19 @@
 import React from 'react';
 import NextLink from 'next/link';
 import { Box, Container, Grid, Text, Flex, Divider } from '@modulz/design-system';
-import { ModulzLogo } from './ModulzLogo';
+import { StitchesLogo } from '../components/StitchesLogo';
 
 const Link = (props) => (
-  <Box as="a" {...props} css={{ color: 'inherit', textDecoration: 'none', ...props.css }} />
+  <Box as="a" {...props} css={{ color: 'gray600', textDecoration: 'none', ...props.css }} />
 );
 
 export const Footer = () => {
   return (
-    <Box css={{ pb: '8' }}>
-      <Divider css={{ mb: '8', width: '100%' }} />
-      <Container size="2" css={{ maxWidth: '1090px' }}>
+    <Box as="footer" css={{ pb: '9' }}>
+      <Flex css={{ justifyContent: 'center', my:'9' }}>
+        <Divider size="large" />
+      </Flex>
+      <Container size="4">
         <Grid
           css={{
             gridTemplateColumns: 'repeat(1, 1fr)',
@@ -34,144 +36,119 @@ export const Footer = () => {
           >
             <NextLink href="/" passHref>
               <Link css={{ color: 'hiContrast', ':focus': { boxShadow: 'none' } }}>
-                <span
-                  style={{
-                    position: 'absolute',
-                    width: 1,
-                    height: 1,
-                    padding: 0,
-                    margin: -1,
-                    overflow: 'hidden',
-                    clip: 'rect(0, 0, 0, 0)',
-                    whiteSpace: 'nowrap',
-                    border: 0,
+                <Box
+                  as="a"
+                  css={{
+                    color: 'hiContrast',
+                    display: 'inline-flex',
+                    ':focus': { 
+                      boxShadow: 'none'
+                    }
                   }}
                 >
-                  Modulz homepage
-                </span>
-                <ModulzLogo aria-hidden />
+                  <span
+                    style={{
+                      position: 'absolute',
+                      width: 1,
+                      height: 1,
+                      padding: 0,
+                      margin: -1,
+                      overflow: 'hidden',
+                      clip: 'rect(0, 0, 0, 0)',
+                      whiteSpace: 'nowrap',
+                      border: 0,
+                    }}
+                  >
+                    Stitches homepage
+                  </span>
+                  <StitchesLogo />
+                </Box>
               </Link>
             </NextLink>
-
-            <Text
-              as="span"
-              size="2"
-              css={{
-                userSelect: 'none',
-                ml: 'auto',
-                bp2: {
-                  ml: '0',
-                  mt: 'auto',
-                },
-              }}
-            >
-              &copy; Modulz, Inc
-            </Text>
           </Flex>
           <Box>
-            <Text as="h6" size="1" css={{ fontWeight: 500 }}>
-              Product
+            <Text as="h6" size="3" css={{ fontWeight: 500, lineHeight: '20px' }}>
+              Overview
             </Text>
             <ul>
               <li>
-                <Text
-                  as="p"
-                  size="3"
-                  css={{ color: 'gray500', mt: '2', lineHeight: '1', userSelect: 'none' }}
-                >
-                  Theme
-                </Text>
-              </li>
-              <li>
-                <Text
-                  as="p"
-                  size="3"
-                  css={{ color: 'gray500', mt: '2', lineHeight: '1', userSelect: 'none' }}
-                >
-                  Editor
-                </Text>
-              </li>
-              <li>
-                <Text
-                  as="p"
-                  size="3"
-                  css={{ color: 'gray500', mt: '2', lineHeight: '1', userSelect: 'none' }}
-                >
-                  Composer
-                </Text>
-              </li>
-              <li>
-                <Text as="p" size="3" css={{ mt: '2', lineHeight: '1' }}>
-                  <NextLink href="/styleguide" passHref>
-                    <Link>Style guide</Link>
+                <Text as="p" size="3" css={{ mt: '3', lineHeight: '20px' }}>
+                  <NextLink href="/docs/introduction" passHref>
+                    <Link>Introduction</Link>
                   </NextLink>
                 </Text>
               </li>
               <li>
-                <Text as="p" size="3" css={{ mt: '2', lineHeight: '1' }}>
-                  <Link href="https://radix.modulz.app">Radix</Link>
+                <Text as="p" size="3" css={{ mt: '3', lineHeight: '20px' }}>
+                  <NextLink href="/docs/releases" passHref>
+                    <Link>Releases</Link>
+                  </NextLink>
                 </Text>
               </li>
               <li>
-                <Text as="p" size="3" css={{ mt: '2', lineHeight: '1' }}>
-                  <Link href="https://modulz.typeform.com/to/fp4gBW">Beta</Link>
+                <Text as="p" size="3" css={{ mt: '3', lineHeight: '20px' }}>
+                  <NextLink href="/docs/tutorials" passHref>
+                    <Link>Tutorials</Link>
+                  </NextLink>
+                </Text>
+              </li>
+              <li>
+                <Text as="p" size="3" css={{ mt: '3', lineHeight: '20px' }}>
+                  <NextLink href="/docs/frequently-asked-questions" passHref>
+                    <Link>Frequently-asked questions</Link>
+                  </NextLink>
+                </Text>
+              </li>
+              <li>
+                <Text as="p" size="3" css={{ mt: '3', lineHeight: '20px' }}>
+                  <NextLink href="/docs/contributing" passHref>
+                    <Link>Contributing</Link>
+                  </NextLink>
                 </Text>
               </li>
             </ul>
           </Box>
           <Box>
-            <Text as="h6" size="1" css={{ fontWeight: 500 }}>
+            <Text as="h6" size="3" css={{ fontWeight: 500, lineHeight: '20px' }}>
+              Docs
+            </Text>
+            <ul>
+              <li>
+                <Text as="p" size="3" css={{ mt: '3', lineHeight: '20px' }}>
+                  <NextLink href="/docs/installation" passHref>
+                    <Link>Installation</Link>
+                  </NextLink>
+                </Text>
+              </li>
+              <li>
+                <Text as="p" size="3" css={{ mt: '3', lineHeight: '20px' }}>
+                  <NextLink href="/docs/tokens" passHref>
+                    <Link>Configuration</Link>
+                  </NextLink>
+                </Text>
+              </li>
+              <li>
+                <Text as="p" size="3" css={{ mt: '3', lineHeight: '20px' }}>
+                  <NextLink href="/docs/api" passHref>
+                    <Link>API</Link>
+                  </NextLink>
+                </Text>
+              </li>
+            </ul>
+          </Box>
+          <Box>
+            <Text as="h6" size="3" css={{ fontWeight: 500, lineHeight: '20px' }}>
               Community
             </Text>
             <ul>
               <li>
-                <Text as="p" size="3" css={{ mt: '3', lineHeight: '1' }}>
-                  <Link href="https://twitter.com/modulz">Twitter</Link>
+                <Text as="p" size="3" css={{ mt: '3', lineHeight: '20px' }}>
+                  <Link href="https://twitter.com/stitchesjs">Twitter</Link>
                 </Text>
               </li>
               <li>
-                <Text as="p" size="3" css={{ mt: '2', lineHeight: '1' }}>
-                  <Link href="https://github.com/modulz">Github</Link>
-                </Text>
-              </li>
-              <li>
-                <Text as="p" size="3" css={{ mt: '2', lineHeight: '1' }}>
-                  <Link href="https://spectrum.chat/modulz">Spectrum</Link>
-                </Text>
-              </li>
-              <li>
-                <Text as="p" size="3" css={{ mt: '2', lineHeight: '1' }}>
-                  <Link href="https://www.youtube.com/channel/UCEU5U-sfF6fmGvTmelBRePQ">
-                    Youtube
-                  </Link>
-                </Text>
-              </li>
-            </ul>
-          </Box>
-          <Box>
-            <Text as="h6" size="1" css={{ fontWeight: 500 }}>
-              Company
-            </Text>
-            <ul>
-              <li>
-                <Text as="p" size="3" css={{ mt: '2', lineHeight: '1' }}>
-                  <NextLink href="/about" passHref>
-                    <Link>About</Link>
-                  </NextLink>
-                </Text>
-              </li>
-              <li>
-                <Text as="p" size="3" css={{ mt: '2', lineHeight: '1' }}>
-                  <NextLink href="/blog" passHref>
-                    <Link>Blog</Link>
-                  </NextLink>
-                </Text>
-              </li>
-              <li>
-                <Text as="p" size="3" css={{ mt: '2', lineHeight: '1' }}>
-                  <NextLink href="/careers" passHref>
-                    <Link>Careers</Link>
-                  </NextLink>
+                <Text as="p" size="3" css={{ mt: '3', lineHeight: '20px' }}>
+                  <Link href="https://github.com/modulz/stitches">Github</Link>
                 </Text>
               </li>
             </ul>
