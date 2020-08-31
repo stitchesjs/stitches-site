27700f057ddde3299351483eeac276e2f0930533
Pedro Duarte <contact@peduarte.com>
2020-08-31 15:38:34 +0200

    Update TitleAndMetaTags.tsx

--- components/TitleAndMetaTags.tsx
+++ components/TitleAndMetaTags.tsx
@@ -30,7 +30,7 @@ export function TitleAndMetaTags({
       <meta property="og:description" content={description} />
       <meta property="og:image" content={image} />
 
-      <meta name="twitter:site" content="@modulz" />
+      <meta name="twitter:site" content="@stitchesjs" />
       <meta name="twitter:card" content="summary" />
       <meta name="twitter:card" content="summary_large_image" />
     </Head>
