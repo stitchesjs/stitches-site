b115e3650d2f65110b8cd07c41c027b2af3d89c3
Pedro Duarte <contact@peduarte.com>
2020-09-01 14:00:16 +0200

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -25,7 +25,7 @@ function App({ Component, pageProps }: AppProps) {
     <MDXProvider components={MDXComponents}>
       <Head>
         <link rel="icon" href="/favicon.png" />
-        <link rel="stylesheet" href="https://core.modulz.app/fonts/fonts.css" />
+        <link rel="stylesheet" href="https://develop.modulz.app/fonts/fonts.css" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <style
           dangerouslySetInnerHTML={{
