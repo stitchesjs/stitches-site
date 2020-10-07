75a01e1a224a0bbe7bcd8d6f2178cca9d3185354
Pedro Duarte <contact@peduarte.com>
2020-10-07 17:22:06 +0200

    Update benchmark.tsx

--- pages/benchmark.tsx
+++ pages/benchmark.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import Head from 'next/head';
 import NextLink from 'next/link';
-import { Box, Container, Text, theme } from '@modulz/design-system';
+import { Badge, Box, Container, Text, Link, Button } from '@modulz/design-system';
 import { StitchesLogo } from '../components/StitchesLogo';
 import { TitleAndMetaTags } from '../components/TitleAndMetaTags';
+import { format } from 'date-fns';
 
 function collectBenchesPerTestCase(entries) {
   const map = new Map();
@@ -22,147 +23,30 @@ function collectBenchesPerTestCase(entries) {
   return map;
 }
 
-export default function BenchmarkPage(props) {
+const fontFamily = 'Untitled Sans, -apple-system, BlinkMacSystemFont, system-ui, sans-serif';
+
+export default function BenchmarkPage({ data, ...props }) {
+  if (!data) {
+    return null;
+  }
+
+  const dataSets = Object.keys(data.entries).map((name) => ({
+    name,
+    dataSet: collectBenchesPerTestCase(data.entries[name]),
+  }));
+
   React.useEffect(() => {
-    // const toolColors = {
-    //   cargo: '#dea584',
-    //   go: '#00add8',
-    //   benchmarkjs: 'hsl(16,60%,74%)',
-    //   pytest: '#3572a5',
-    //   googlecpp: '#f34b7d',
-    //   catch2: '#f34b7d',
-    //   _: '#333333',
-    // };
-
-    function init() {
-      const data = props.data;
-      // Prepare data points for charts
-      return Object.keys(data.entries).map((name) => ({
-        name,
-        dataSet: collectBenchesPerTestCase(data.entries[name]),
-      }));
-    }
-
-    function renderAllChars(dataSets) {
-      function renderGraph(parent, name, dataset) {
-        const canvas = document.createElement('canvas');
-        canvas.className = 'benchmark-chart';
-        parent.appendChild(canvas);
-
-        // const color = toolColors[dataset.length > 0 ? dataset[0].tool : '_'];
-        const data = {
-          labels: dataset.map((d) => d.commit.id.slice(0, 7)),
-          datasets: [
-            {
-              label: name,
-              data: dataset.map((d) => d.bench.value),
-              // borderColor: color,
-              // backgroundColor: color + '60', // Add alpha for #rrggbbaa
-              borderColor: 'hsl(252,80%,70%)',
-              backgroundColor: 'hsl(272,100%,96%)',
-            },
-          ],
-        };
-        const options = {
-          scales: {
-            xAxes: [
-              {
-                scaleLabel: {
-                  display: true,
-                  labelString: 'commit',
-                },
-              },
-            ],
-            yAxes: [
-              {
-                scaleLabel: {
-                  display: true,
-                  labelString: dataset.length > 0 ? dataset[0].bench.unit : '',
-                },
-                ticks: {
-                  beginAtZero: true,
-                },
-              },
-            ],
-          },
-          tooltips: {
-            callbacks: {
-              afterTitle: (items) => {
-                const { index } = items[0];
-                const data = dataset[index];
-                return (
-                  '\n' +
-                  data.commit.message +
-                  '\n\n' +
-                  data.commit.timestamp +
-                  ' committed by @' +
-                  data.commit.committer.username +
-                  '\n'
-                );
-              },
-              label: (item) => {
-                let label = item.value;
-                const { range, unit } = dataset[item.index].bench;
-                label += ' ' + unit;
-                if (range) {
-                  label += ' (' + range + ')';
-                }
-                return label;
-              },
-              afterLabel: (item) => {
-                const { extra } = dataset[item.index].bench;
-                return extra ? '\n' + extra : '';
-              },
-            },
-          },
-          onClick: (_mouseEvent, activeElems) => {
-            if (activeElems.length === 0) {
-              return;
-            }
-            // XXX: Undocumented. How can we know the index?
-            const index = activeElems[0]._index;
-            const url = dataset[index].commit.url;
-            window.open(url, '_blank');
-          },
-        };
-
-        (window as any).Chart.defaults.global.defaultFontFamily =
-          'Untitled Sans, -apple-system, BlinkMacSystemFont, system-ui, sans-serif';
-
-        new (window as any).Chart(canvas, {
-          type: 'line',
-          data,
-          options,
-        });
-      }
-
-      function renderBenchSet(name, benchSet, main) {
-        const setElem = document.createElement('div');
-        setElem.className = 'benchmark-set';
-        main.appendChild(setElem);
-
-        const graphsElem = document.createElement('div');
-        graphsElem.className = 'benchmark-graphs';
-        setElem.appendChild(graphsElem);
-
-        for (const [benchName, benches] of benchSet.entries()) {
-          renderGraph(graphsElem, benchName, benches);
-        }
-      }
-
-      const main = document.getElementById('benchmark-main');
-      for (const { name, dataSet } of dataSets) {
-        renderBenchSet(name, dataSet, main);
-      }
-    }
-    renderAllChars(init()); // Start
+    // Global Chart.js overrides
+    (window as any).Chart.defaults.global.defaultFontFamily = fontFamily;
+    (window as any).Chart.defaults.global.animation.duration = 0;
   }, []);
+
   return (
     <Box>
       <Head>
-        <script src="https://cdn.jsdelivr.net/npm/chart.js@2.9.2/dist/Chart.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/chart.js@2.9.3/dist/Chart.min.js"></script>
       </Head>
-      <TitleAndMetaTags title="Stitches" />
+      <TitleAndMetaTags title="Stitches Benchmarks" />
       <Box as="header" css={{ py: '$4', px: '$4', mb: '$7' }}>
         <NextLink href="/" passHref>
           <Box
@@ -196,23 +80,170 @@ export default function BenchmarkPage(props) {
       </Box>
       <Container size="3" css={{ textAlign: 'center', mb: '$4' }}>
         <Text
+          as="h1"
           size={{ initial: '8', bp2: '9' }}
-          css={{ fontWeight: 500, lineHeight: '35px', bp2: { lineHeight: '55px' } }}
+          css={{
+            fontWeight: 500,
+            lineHeight: '35px',
+            mb: '$5',
+            bp2: { lineHeight: '55px' },
+          }}
         >
           Stitches Benchmarks
         </Text>
 
-        <Box
-          css={{
-            py: '$7',
-            '.benchmark-chart': {
-              mb: '$7',
-            },
-          }}
+        <Text as="div" css={{ mb: '$3' }}>
+          Updated: {format(data.lastUpdate, 'dd/MM/yy')} at {format(data.lastUpdate, 'HH:mm')}{' '}
+          <Badge>{format(data.lastUpdate, 'O')}</Badge>
+        </Text>
+
+        {dataSets.map(({ name, dataSet }) => (
+          <Bench key={name} name={name} dataSet={dataSet} />
+        ))}
+
+        <Text as="div" css={{ mb: '$5' }}>
+          Repository: <Link href={data.repoUrl}>{data.repoUrl}</Link>
+        </Text>
+
+        <Button
+          as="a"
+          href={`data:,${JSON.stringify(data, null, 2)}`}
+          download="benchmark_data.json"
+          css={{ mb: '$7' }}
         >
-          <main id="benchmark-main"></main>
-        </Box>
+          Download data
+        </Button>
       </Container>
+    </Box>
+  );
+}
+
+function Bench(props: any) {
+  return (
+    <>
+      {[...props.dataSet.entries()].map(([name, data], i) => (
+        <Chart key={i} name={name} dataSet={data} />
+      ))}
+    </>
+  );
+}
+
+function Chart({ dataSet, name }: any) {
+  const canvasRef = React.useRef<HTMLCanvasElement>(null);
+
+  React.useEffect(() => {
+    const data = {
+      labels: dataSet.map((d) => d.commit.id.slice(0, 7)),
+      datasets: [
+        {
+          label: name,
+          data: dataSet.map((d) => d.bench.value),
+
+          borderColor: 'hsl(252,80%,70%)',
+          backgroundColor: 'hsla(252,80%,70%, 0.2)',
+          pointBackgroundColor: 'hsl(252,80%,70%)',
+          pointBorderColor: 'white',
+          pointRadius: 5,
+          pointHoverRadius: 5,
+          pointHitRadius: 3,
+        },
+      ],
+    };
+    const options = {
+      global: {
+        defaultFontFamily: fontFamily,
+      },
+      hover: {
+        animationDuration: 0,
+      },
+      scales: {
+        xAxes: [
+          {
+            gridLines: {
+              color: 'hsla(252,80%,70%, 0.4)',
+              zeroLineColor: 'hsla(252,80%,70%, 0.4)',
+            },
+            scaleLabel: {
+              display: true,
+              labelString: 'commit',
+            },
+          },
+        ],
+        yAxes: [
+          {
+            gridLines: {
+              color: 'hsla(252,80%,70%, 0.4)',
+              zeroLineColor: 'hsla(252,80%,70%, 0.4)',
+            },
+            scaleLabel: {
+              display: true,
+              labelString: dataSet.length > 0 ? dataSet[0].bench.unit : '',
+            },
+            ticks: {
+              beginAtZero: true,
+            },
+          },
+        ],
+      },
+      tooltips: {
+        displayColors: false,
+        xPadding: 15,
+        yPadding: 15,
+        titleFontFamily: fontFamily,
+        bodyFontFamily: fontFamily,
+        footerFontFamily: fontFamily,
+        titleSpacing: 5,
+        titleMarginBottom: 10,
+        bodySpacing: 5,
+        bodyMarginBottom: 0,
+        cornerRadius: 5,
+        callbacks: {
+          afterTitle: (items) => {
+            const { index } = items[0];
+            return dataSet[index].commit.message;
+          },
+          label: (item) => {
+            let label = item.value;
+            const { range, unit } = dataSet[item.index].bench;
+            label += ' ' + unit;
+            if (range) {
+              label += ' (' + range + ')';
+            }
+            return label;
+          },
+          afterLabel: (item) => {
+            const { extra } = dataSet[item.index].bench;
+            return extra ? extra : '';
+          },
+        },
+      },
+      onClick: (_mouseEvent, activeElems) => {
+        if (activeElems.length === 0) {
+          return;
+        }
+        const index = activeElems[0]._index;
+        const url = dataSet[index].commit.url;
+        window.open(url, '_blank');
+      },
+    };
+
+    new (window as any).Chart(canvasRef.current, {
+      type: 'line',
+      data,
+      options,
+    });
+  }, [dataSet]);
+
+  return (
+    <Box
+      css={{
+        py: '$7',
+        '.benchmark-chart': {
+          mb: '$7',
+        },
+      }}
+    >
+      <canvas ref={canvasRef}></canvas>
     </Box>
   );
 }
