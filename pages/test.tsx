ad3fc23b44394b3cdc1b21ec5ae933515f0db1d4
Pedro Duarte <contact@peduarte.com>
2021-03-19 09:26:49 +0100

    Pre variants

--- pages/test.tsx
+++ pages/test.tsx
@@ -3,6 +3,7 @@ import { Box, Code, Text, Paragraph, Card, Grid, Container } from '@modulz/desig
 import { TitleAndMetaTags } from '@components/TitleAndMetaTags';
 import { Header } from '@components/Header';
 import { CodeDemo } from '@components/CodeDemo';
+import { CodeBlock } from '@components/CodeBlock';
 
 const demoCode = `const Button = styled('button', {
   appearance: 'none'
@@ -53,84 +54,22 @@ export default function Test() {
 
       <Header />
 
-      <Container size="3" css={{ py: '$8' }}>
-        <Grid
-          css={{
-            gap: '$9',
-            ai: 'center',
-            when: {
-              initial: { gridTemplateColumns: 'repeat(1, 1fr)' },
-              bp2: { gridTemplateColumns: 'repeat(2, 1fr)' },
-            },
-          }}
-        >
-          <Box>
-            <Box css={{ mb: '$5' }}>
-              <Code css={{ mb: '$5', fontFamily: '$mono', fontSize: '$4' }}>Variants</Code>
-            </Box>
-            <Text size="8" css={{ fontWeight: 500, lineHeight: '40px', mb: '$4' }}>
-              Design composable component APIs with variants
-            </Text>
-            <Paragraph css={{ mb: '$6' }}>
-              Variants are a first-class citizen of Stitches. With multiple variants, compound
-              variants, and default variants, you can design composable component APIs which are
-              typed automatically.
-            </Paragraph>
-            <Box css={{ mx: '-$3' }}>
-              <Card
-                as="button"
-                onMouseDown={() => setActiveHighlight('one')}
-                variant={activeHighlight === 'one' ? 'active' : 'ghost'}
-                css={{ p: '$3', mb: '$2', cursor: 'default' }}
-              >
-                <Text css={{ fontWeight: 500, lineHeight: '20px', mb: '$1' }}>Base styles</Text>
-                <Text variant="gray" css={{ lineHeight: '20px' }}>
-                  Apply styles when two or more variants match.
-                </Text>
-              </Card>
-              <Card
-                as="button"
-                onMouseDown={() => setActiveHighlight('two')}
-                variant={activeHighlight === 'two' ? 'active' : 'ghost'}
-                css={{ p: '$3', mb: '$2', cursor: 'default' }}
-              >
-                <Text css={{ fontWeight: 500, lineHeight: '20px', mb: '$1' }}>Variants</Text>
-                <Text variant="gray" css={{ lineHeight: '20px' }}>
-                  Apply styles when two or more variants match.
-                </Text>
-              </Card>
-              <Card
-                as="button"
-                onMouseDown={() => setActiveHighlight('three')}
-                variant={activeHighlight === 'three' ? 'active' : 'ghost'}
-                css={{ p: '$3', mb: '$2', cursor: 'default' }}
-              >
-                <Text css={{ fontWeight: 500, lineHeight: '20px', mb: '$1' }}>
-                  Default Variants
-                </Text>
-                <Text variant="gray" css={{ lineHeight: '20px' }}>
-                  Apply styles when two or more variants match.
-                </Text>
-              </Card>
-            </Box>
-          </Box>
-          <Box
-            css={{
-              position: 'relative',
-              alignSelf: 'start',
-              mt: 50,
-            }}
-          >
-            <CodeDemo
-              language="jsx"
-              variant="indigo"
-              value={demoCode}
-              line={highlightMap[activeHighlight]}
-              css={{ height: 600 }}
-            />
-          </Box>
-        </Grid>
-      </Container>
+      <Grid
+        css={{
+          gap: '$9',
+          ai: 'center',
+          p: '$9',
+          when: {
+            initial: { gridTemplateColumns: 'repeat(1, 1fr)' },
+            bp2: { gridTemplateColumns: 'repeat(4, 1fr)' },
+          },
+        }}
+      >
+        <CodeBlock language="jsx" value={demoCode} />
+        <CodeBlock variant="indigo" language="jsx" value={demoCode} />
+        <CodeBlock variant="yellow" language="jsx" value={demoCode} />
+        <CodeBlock variant="pink" language="jsx" value={demoCode} />
+      </Grid>
     </Box>
   );
 }
