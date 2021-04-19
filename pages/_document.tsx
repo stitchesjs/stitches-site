7217d90ebd17deb2fb3eccddba36a519a86a151a
Jonathan Neal <jonathantneal@hotmail.com>
2021-04-19 16:40:28 -0400

    Update <head> tag ordering

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,10 +8,9 @@ export default class Document extends NextDocument {
     return (
       <Html lang="en">
         <Head>
+          <link rel="stylesheet" href="https://develop.modulz.app/fonts/fonts.css" />
           <style id="stitches" dangerouslySetInnerHTML={{ __html: getCssString() }} />
           <script dangerouslySetInnerHTML={{ __html: renderSnippet() }} />
-          <link rel="icon" href="/favicon.png" />
-          <link rel="stylesheet" href="https://develop.modulz.app/fonts/fonts.css" />
         </Head>
         <body>
           <Main />
