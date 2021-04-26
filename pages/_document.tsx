234773660fdf8701a30148081058158da52e0081
Pedro Duarte <contact@peduarte.com>
2021-04-26 20:51:23 +0200

    Update font loading strategy

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,12 +9,12 @@ export default class Document extends NextDocument {
       <Html lang="en">
         <Head>
           <style id="stitches" dangerouslySetInnerHTML={{ __html: getCssString() }} />
-          <link
-            href="https://develop.modulz.app/fonts/fonts.css"
-            rel="preload"
-            as="font"
-            crossOrigin="anonymous"
-          />
+          <link rel="preconnect" href="https://develop.modulz.app" crossOrigin="anonymous" />
+          <link rel="preload" href="https://develop.modulz.app/fonts/fonts.css" as="style" />
+          <link rel="stylesheet" href="https://develop.modulz.app/fonts/fonts.css" media="all" />
+          <noscript>
+            <link rel="stylesheet" href="https://develop.modulz.app/fonts/fonts.css" />
+          </noscript>
           <script dangerouslySetInnerHTML={{ __html: renderSnippet() }} />
         </Head>
         <body>
