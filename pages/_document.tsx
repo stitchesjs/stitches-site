48159f7910e345e3eae55a9f5d0c28564cbbf890
Pedro Duarte <contact@peduarte.com>
2021-04-27 14:31:36 +0200

    Load fonts locally

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,13 +9,101 @@ export default class Document extends NextDocument {
       <Html lang="en">
         <Head>
           <style id="stitches" dangerouslySetInnerHTML={{ __html: getCssString() }} />
-          <link rel="preconnect" href="https://develop.modulz.app" crossOrigin="anonymous" />
-          <link rel="preload" href="https://develop.modulz.app/fonts/fonts.css" as="style" />
-          <link rel="stylesheet" href="https://develop.modulz.app/fonts/fonts.css" media="all" />
-          <noscript>
-            <link rel="stylesheet" href="https://develop.modulz.app/fonts/fonts.css" />
-          </noscript>
+          <link
+            rel="preload"
+            href="/fonts/UntitledSansWeb-Regular.woff"
+            as="font"
+            type="font/woff"
+            crossOrigin="anonymous"
+          />
+          <link
+            rel="preload"
+            href="/fonts/UntitledSansWeb-Regular.woff2"
+            as="font"
+            type="font/woff2"
+            crossOrigin="anonymous"
+          />
+          <link
+            rel="preload"
+            href="/fonts/UntitledSansWeb-Medium.woff"
+            as="font"
+            type="font/woff"
+            crossOrigin="anonymous"
+          />
+          <link
+            rel="preload"
+            href="/fonts/UntitledSansWeb-Medium.woff2"
+            as="font"
+            type="font/woff2"
+            crossOrigin="anonymous"
+          />
+          <link
+            rel="preload"
+            href="/fonts/soehne-mono-web-buch.woff"
+            as="font"
+            type="font/woff"
+            crossOrigin="anonymous"
+          />
+          <link
+            rel="preload"
+            href="/fonts/soehne-mono-web-buch.woff2"
+            as="font"
+            type="font/woff2"
+            crossOrigin="anonymous"
+          />
           <script dangerouslySetInnerHTML={{ __html: renderSnippet() }} />
+          <style
+            dangerouslySetInnerHTML={{
+              __html: `
+@font-face {
+  font-family: 'Untitled Sans';
+  font-weight: 400;
+  font-display: swap;
+  src: url(/fonts/UntitledSansWeb-Regular.woff2) format('woff2'), url(/fonts/UntitledSansWeb-Regular.woff) format('woff');
+}
+
+@font-face {
+  font-family: 'Untitled Sans';
+  font-weight: 500;
+  font-display: swap;
+  src: url(/fonts/UntitledSansWeb-Medium.woff2) format('woff2'), url(/fonts/UntitledSansWeb-Medium.woff) format('woff');
+}
+
+@font-face {
+  font-family: 'Söhne Mono';
+  font-weight: normal;
+  font-style: normal;
+  font-display: swap;
+  src: url(/fonts/'soehne-mono-web-buch.woff2') format('woff2'), url(/fonts/'soehne-mono-web-buch.woff') format('woff');
+}
+
+@font-face {
+  font-family: 'Söhne Mono';
+  font-weight: normal;
+  font-style: italic;
+  font-display: swap;
+  src: url(/fonts/'soehne-mono-web-buch-kursiv.woff2') format('woff2'), url(/fonts/'soehne-mono-web-buch-kursiv.woff') format('woff');
+}
+
+@font-face {
+  font-family: 'Söhne Mono';
+  font-weight: 500;
+  font-style: normal;
+  font-display: swap;
+  src: url(/fonts/'soehne-mono-web-kraftig.woff2') format('woff2'), url(/fonts/'soehne-mono-web-kraftig.woff') format('woff');
+}
+
+@font-face {
+  font-family: 'Söhne Mono';
+  font-weight: 500;
+  font-style: italic;
+  font-display: swap;
+  src: url(/fonts/'soehne-mono-web-kraftig-kursiv.woff2') format('woff2'),
+    url(/fonts/'soehne-mono-web-kraftig-kursiv.woff') format('woff');
+}
+					`,
+            }}
+          />
         </Head>
         <body>
           <Main />
