d987fd12d255a4db62f37d1516f105a7953c4ff9
Colm Tuite <colmtuite@gmail.com>
2021-06-14 17:28:17 +0100

    Update charts

--- components/BenchmarkChart.tsx
+++ components/BenchmarkChart.tsx
@@ -4,18 +4,21 @@ export function BenchmarkChart({ data }) {
   const maxValue = Math.max(...data.map((r) => r.value));
 
   return (
-    <Flex css={{ bc: '$violet2', p: '$6', gap: '$2', mb: '$4' }}>
+    <Flex css={{ gap: '$2', mt: '$4' }}>
       <Box css={{ flex: '0 1 auto' }}>
         {data.map((result) => (
           <Text
             key={result.name}
             size="1"
             css={{
-              my: '$2',
+              my: '$1',
               lineHeight: '$sizes$5',
               whiteSpace: 'nowrap',
-              color: '$violet9',
               ta: 'right',
+              fontWeight:
+                result.name === 'Stitches' || result.name === 'Stitches 0.2.0'
+                  ? '500'
+                  : '400',
             }}
           >
             {result.name}
@@ -24,7 +27,7 @@ export function BenchmarkChart({ data }) {
       </Box>
       <Box css={{ flex: '1 1 100%' }}>
         {data.map((result) => (
-          <Flex key={result.name} css={{ alignItems: 'center', gap: '$2', my: '$2' }}>
+          <Flex key={result.name} css={{ alignItems: 'center', gap: '$2', my: '$1' }}>
             <Flex
               css={{
                 height: '$5',
@@ -39,11 +42,11 @@ export function BenchmarkChart({ data }) {
                   bc:
                     result.name === 'Stitches' || result.name === 'Stitches 0.2.0'
                       ? '$violet9'
-                      : '$violet4',
+                      : '$violet9',
                   width: `${(result.value / maxValue) * 100}%`,
                 }}
               />
-              <Text size="1" css={{ whiteSpace: 'nowrap', color: '$violet9' }}>
+              <Text size="1" css={{ whiteSpace: 'nowrap' }}>
                 {result.value}ms
               </Text>
             </Flex>
