e0d3da3b316ce2b4f2e92015734855d504dcdbc0
Pedro Duarte <contact@peduarte.com>
2021-06-14 17:44:43 +0200

    Update stuff

--- components/BenchmarkChart.tsx
+++ components/BenchmarkChart.tsx
@@ -4,34 +4,52 @@ export function BenchmarkChart({ data }) {
   const maxValue = Math.max(...data.map((r) => r.value));
 
   return (
-    <Box css={{ bc: '$violet2', p: '$6', mb: '$4' }}>
-      {data.map((result) => (
-        <Flex key={result.name} css={{ alignItems: 'center', gap: '$2' }}>
-          <Text size="1" css={{ whiteSpace: 'nowrap', color: '$violet9', ta: 'right', width: 110 }}>
-            {result.name}
-          </Text>
-          <Flex
+    <Flex css={{ bc: '$violet2', p: '$6', gap: '$2', mb: '$4' }}>
+      <Box css={{ flex: '0 1 auto' }}>
+        {data.map((result) => (
+          <Text
+            key={result.name}
+            size="1"
             css={{
               my: '$2',
-              height: '$5',
-              flex: '1',
-              alignItems: 'center',
-              gap: '$2',
+              lineHeight: '$sizes$5',
+              whiteSpace: 'nowrap',
+              color: '$violet9',
+              ta: 'right',
             }}
           >
-            <Box
+            {result.name}
+          </Text>
+        ))}
+      </Box>
+      <Box css={{ flex: '1 1 100%' }}>
+        {data.map((result) => (
+          <Flex key={result.name} css={{ alignItems: 'center', gap: '$2', my: '$2' }}>
+            <Flex
               css={{
-                height: '100%',
-                bc: result.name.includes('Stitches') ? '$violet9' : '$violet4',
-                width: `${(result.value / maxValue) * 100}%`,
+                height: '$5',
+                flex: '1',
+                alignItems: 'center',
+                gap: '$2',
               }}
-            />
-            <Text size="1" css={{ whiteSpace: 'nowrap', color: '$violet9' }}>
-              {result.value}ms
-            </Text>
+            >
+              <Box
+                css={{
+                  height: '100%',
+                  bc:
+                    result.name === 'Stitches' || result.name === 'Stitches 0.2.0'
+                      ? '$violet9'
+                      : '$violet4',
+                  width: `${(result.value / maxValue) * 100}%`,
+                }}
+              />
+              <Text size="1" css={{ whiteSpace: 'nowrap', color: '$violet9' }}>
+                {result.value}ms
+              </Text>
+            </Flex>
           </Flex>
-        </Flex>
-      ))}
-    </Box>
+        ))}
+      </Box>
+    </Flex>
   );
 }
