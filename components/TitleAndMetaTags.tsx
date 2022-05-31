466c0160d7bbb624e62dbee404d8783568e4e130
Andy Hook <hello@andyhook.dev>
2022-05-31 19:15:36 +0100

    Rename

--- components/TitleAndMetaTags.tsx
+++ components/TitleAndMetaTags.tsx
@@ -14,7 +14,7 @@ export function TitleAndMetaTags({
   url = 'https://stitches.dev',
   pathname,
   title = 'Stitches',
-  description = 'CSS-in-JS with near-zero runtime, SSR, multi-variant support, and a best-in-class developer experience, by Modulz.',
+  description = 'CSS-in-JS with near-zero runtime, SSR, multi-variant support, and a best-in-class developer experience, by WorkOS.',
   poster,
 }: TitleAndMetaTagsProps) {
   const router = useRouter();
